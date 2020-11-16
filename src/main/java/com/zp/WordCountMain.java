f124f70176ec2a948d6e4b07a49247f8b8ad0d4f
zhoupeng <287214764@qq.com>
2020-11-16 13:07:41 +0800

    提交到storm集群中执行

--- src/main/java/com/zp/WordCountMain.java
+++ src/main/java/com/zp/WordCountMain.java
@@ -2,6 +2,7 @@ package com.zp;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.utils.Utils;
@@ -16,26 +17,33 @@ public class WordCountMain {
         topologyBuilder.setSpout("RandomSentenceSpout", new com.zp.RandomSentenceSpout(), 2);
         // 10表示executor的个数，20表示task的个数
         topologyBuilder.setBolt("SplitSentenceBlot", new SplitSentenceBlot(), 5)
-                                .setNumTasks(10)
-                                .shuffleGrouping("RandomSentenceSpout");
+                .setNumTasks(10)
+                .shuffleGrouping("RandomSentenceSpout");
         // 设置fieldsGrouping，相同单词会进到同一个task中,保证单词统计的准确性
         topologyBuilder.setBolt("WordCountBolt", new WordCountBolt(), 10)
-                                .setNumTasks(20)
-                                .fieldsGrouping("SplitSentenceBlot", new Fields("word"));
+                .setNumTasks(20)
+                .fieldsGrouping("SplitSentenceBlot", new Fields("word"));
 
         Config config = new Config();
         // 设置最大并行度
         config.setMaxTaskParallelism(3);
-        // 本地运行
-        LocalCluster localCluster = new LocalCluster();
-        localCluster.submitTopology("WordCount", config, topologyBuilder.createTopology());
-        Utils.sleep(60000);
-        localCluster.shutdown();
+        if (args != null && args.length > 0) {
+            // 说明是在命令行执行，打算提交到storm集群上去
+            config.setNumWorkers(3);
+            try {
+                StormSubmitter.submitTopology(args[0], config, topologyBuilder.createTopology());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            // 本地运行
+            LocalCluster localCluster = new LocalCluster();
+            localCluster.submitTopology("WordCount", config, topologyBuilder.createTopology());
+            Utils.sleep(60000);
+            localCluster.shutdown();
+        }
+
     }
-
-
-
-
 
 
 }
